a44372f536c9d33fd0da9fd2ebc8bf3bac5cb0f9
Adrien Lepoutre <thewebsdoor@gmail.com>
2020-06-12 17:09:37 -0400

    Deploy

---
+++ precache-manifest.53c786f482f5139e01f301975d36fb88.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/PixelsDrawing/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b2140fdd791abad2768a06eb31166662",
+    "url": "/PixelsDrawing/site.webmanifest"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/PixelsDrawing/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "8d5fdf9e7291d7fcf85e",
+    "url": "/PixelsDrawing/css/chunk-vendors.2c2a0273.css"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/PixelsDrawing/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/PixelsDrawing/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "cbc5ec6cccfeef21be3f",
+    "url": "/PixelsDrawing/js/app.4f3c6827.js"
+  },
+  {
+    "revision": "8d5fdf9e7291d7fcf85e",
+    "url": "/PixelsDrawing/js/chunk-vendors.b09f8c69.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/PixelsDrawing/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "67fd5a0ab31e30f06b7569021a0260aa",
+    "url": "/PixelsDrawing/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/PixelsDrawing/robots.txt"
+  },
+  {
+    "revision": "cbc5ec6cccfeef21be3f",
+    "url": "/PixelsDrawing/css/app.bf029eed.css"
+  },
+  {
+    "revision": "64847bbdce0224419ebe5a8485155c43",
+    "url": "/PixelsDrawing/browserconfig.xml"
+  }
+];
